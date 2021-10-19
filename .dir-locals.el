88fdf2a1591bfb30ce5c94146f9dfb5a91daa166
Evan Niessen-Derry <eniessenderry@gmail.com>
2021-10-19 12:27:01 -0500

    Add another option for shadow startup

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
 ((nil . ((cider-default-cljs-repl . shadow)
 	 (cider-shadow-default-options . ":main")
-	 (cider-shadow-watched-builds . (":main")))))
+	 (cider-shadow-watched-builds . (":main"))
+	 (cider-preferred-build-tool . shadow-cljs))))
