c43cb5ea71b2b95f58df628ffa1a9639ba6a6aa5
Evan Niessen-Derry <eniessenderry@gmail.com>
2021-10-19 10:36:50 -0500

    Base set of working files
    This is the most barebones possible app, committing here to track
    newer changes.

---
+++ src/main/app/client.cljs
@@ -0,0 +1,24 @@
+(ns app.client
+  (:require [com.fulcrologic.fulcro.application :as app]
+            [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
+            [com.fulcrologic.fulcro.dom :as dom]))
+
+(defonce app (app/fulcro-app))
+
+(defsc Root [this props]
+  (dom/div "TODO"))
+
+(defn ^:export init
+  "This is our shadow-cljs entry point. See `shadow-cljs.edn` for how
+  this is configured"
+  []
+  (app/mount! app Root "app")
+  (js/console.log "Loaded"))
+
+(defn ^:export refresh
+  "This is a dev helper that shadow-cljs will call on every hot-reload
+  of the source code."
+  []
+  (app/mount! app Root "app")
+  (comp/refresh-dynamic-queries! app)
+  (js/console.log "Hot reload"))
