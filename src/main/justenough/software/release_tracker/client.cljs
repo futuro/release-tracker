893676f9b4f5fe5c77c938c86db6271fd433f8f5
Evan Niessen-Derry <eniessenderry@gmail.com>
2021-10-19 12:31:32 -0500

    Give the namespace root a proper name
    I'd forgotten the app before.

--- src/main/justenough/software/release_tracker/client.cljs
+++ src/main/justenough/software/release_tracker/client.cljs
@@ -1,4 +1,4 @@
-(ns justenough.software.client
+(ns justenough.software.release-tracker.client
   (:require [com.fulcrologic.fulcro.application :as app]
             [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
             [com.fulcrologic.fulcro.dom :as dom]))
