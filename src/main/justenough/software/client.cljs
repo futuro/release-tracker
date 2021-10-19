1ec98b02c3abc375cbc6ae32b78efdcdee6642a0
Evan Niessen-Derry <eniessenderry@gmail.com>
2021-10-19 12:26:46 -0500

    Update namespace name
    Actually, this is still not great.

--- src/main/justenough/software/client.cljs
+++ src/main/justenough/software/client.cljs
@@ -1,4 +1,4 @@
-(ns app.client
+(ns justenough.software.client
   (:require [com.fulcrologic.fulcro.application :as app]
             [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
             [com.fulcrologic.fulcro.dom :as dom]))
