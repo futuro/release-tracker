d711440d25e3e6af28055489f2ee56404c69037e
Evan Niessen-Derry <eniessenderry@gmail.com>
2021-10-20 10:30:37 -0500

    Add new repo search ui file
    Currently it supports updating the input form, and needs work around
    triggering the search, as well as showing the results.

--- src/main/justenough/software/release_tracker/ui.cljs
+++ src/main/justenough/software/release_tracker/ui.cljs
@@ -1,11 +1,16 @@
 (ns justenough.software.release-tracker.ui
   (:require [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
             [com.fulcrologic.fulcro.dom :as dom]
-            [justenough.software.release-tracker.ui.user :as user]))
+            [justenough.software.release-tracker.ui.user :as user]
+            [justenough.software.release-tracker.ui.repo :as repo]))
 
-(defsc Root [this {:github/keys [client user]}]
+(defsc Root [this {:github/keys [client user]
+                   {search :repo/search-form} :component/id
+                   :as props}]
   {:query [:github/client
-           {:github/user (comp/get-query user/User)}]}
+           {:github/user (comp/get-query user/User)}
+           {:component/id {:repo/search-form (comp/get-query repo/SearchForm)}}]}
   (dom/div
    (dom/div "Root div")
-   (user/factory user)))
+   (user/factory user)
+   (repo/search-ui search)))
---
+++ src/main/justenough/software/release_tracker/ui/repo.cljs
@@ -0,0 +1,84 @@
+(ns justenough.software.release-tracker.ui.repo
+  (:require [com.fulcrologic.fulcro.components :as comp :refer [defsc]]
+            [com.fulcrologic.fulcro.dom :as dom]
+            [com.fulcrologic.fulcro.dom.events :as evt]
+            [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]
+            [com.fulcrologic.fulcro.algorithms.form-state :as fs]))
+
+(defn field [{:keys [label valid? error-message] :as props}]
+  (let [input-props (-> props
+                        (assoc :name label)
+                        (dissoc :label :valid? :error-message))]
+    (dom/div :.ui.field
+             (dom/label {:htmlFor label} label)
+             (dom/input input-props)
+             (dom/div :.ui.error.message {:classes [(when valid? "hidden")]}
+                      error-message))))
+
+(defn prepend-keys
+  [m prefix]
+  (reduce-kv (fn [m k v]
+               (assoc m (keyword prefix k) v))
+             {}
+             m))
+
+(defn parse-search-results
+  "Given the results from the Octokit library, returns the list of
+  returned repos as EDN, with each key keywordized, and prepended with
+  `repo`."
+  [results]
+  (let [data (-> results
+                 (js->clj :keywordize-keys true)
+                 :data)]
+    (map #(prepend-keys % "repo")
+         data)))
+
+(defmutation search-repos! [{:repo/keys [name]}]
+  (action [{:keys [state]}]
+    (let [client (:github/client @state)]
+      (try
+        (-> client
+            .-rest
+            .-search
+            (.repos #js {:q name})
+            js/Promise.resolve
+            (.then #(swap! state assoc :repos/list (parse-search-results %))))
+        (catch js/Object o
+          (js/console.log "Search failed with error" o))))))
+
+(declare SearchForm)
+
+(def repo-search-ident
+  [:component/id :repo/search-form])
+
+(defn configure-search-form*
+  [state-map]
+  (-> state-map
+      (assoc-in repo-search-ident
+                {:repo/name ""})
+      (fs/add-form-config* SearchForm repo-search-ident)))
+
+(defmutation configure-search-form [_]
+  (action [{:keys [state]}]
+          (swap! state configure-search-form*)))
+
+(defsc SearchForm [this {:repo/keys [name] :as props}]
+  {:query             [:repo/name fs/form-config-join]
+   :form-fields       #{:repo/name}
+   :ident             (fn [] repo-search-ident)
+   :componentDidMount (fn [this]
+                        (comp/transact! this [(configure-search-form)]))}
+  (let [submit!  (fn [evt]
+                   (when (or (identical? true evt) (evt/enter-key? evt))
+                     (comp/transact! this [(search-repos! {:name name})])))
+        checked? (fs/checked? props)]
+    (dom/div
+     (dom/h3 "Search")
+     (dom/div :.ui.form {:classes [(when checked? "error")]}
+              (dom/input {:label        "Repo name"
+                          :value        (or name "")
+                          :autoComplete "off"
+                          :onKeyDown    submit!
+                          :onChange     #(m/set-string! this :repo/name :event %)})))))
+
+(def search-ui (comp/factory SearchForm))
