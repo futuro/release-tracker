1e51d25414588f9f79348f3fcadab73447c4c5a2
Evan Niessen-Derry <eniessenderry@gmail.com>
2021-10-25 11:59:31 -0500

    Finish main config setup
    The server isn't running, but the configuration is all hooked up.

--- src/main/justenough/software/release_tracker/config.clj
+++ src/main/justenough/software/release_tracker/config.clj
@@ -1,16 +1,21 @@
 (ns justenough.software.release-tracker.config
   (:require [mount.core :refer [args defstate]]
-            [com.fulcrologic.fulcro.server.config :as fsc]
-            [taoensso.timbre :as log]))
+            [aero.core :as aero]
+            [taoensso.timbre :as log]
+            [clojure.java.io :as io]))
 
 (defn configure-logging! [{::log/keys [logging-config]}]
   (log/info "Configuring Timbre with " logging-config)
   (log/merge-config! logging-config))
 
 (defstate config
-  :start (let [{:keys [config]
-                :or {config "config/dev.edn"}} (args)
-               configuration (fsc/load-config! {:config-path config})]
-           (log/info "Loaded config" config)
-           (configure-logging!)
+  :start (let [{:keys [env]
+                :or   {env :dev}} (args)
+               configuration    (-> "config/default.edn"
+                                    io/resource
+                                    (aero/read-config
+                                     {:profile env
+                                      :resolver aero/resource-resolver}))]
+           (log/info "Loaded config")
+           (configure-logging! configuration)
            configuration))
--- src/main/justenough/software/release_tracker/server.clj
+++ src/main/justenough/software/release_tracker/server.clj
@@ -1,6 +1,7 @@
 (ns justenough.software.release-tracker.server
   (:require [justenough.software.release-tracker.config :as cfg]
-            [mount.core :as mount :refer [defstate]]))
+            [mount.core :as mount :refer [defstate]]
+            [org.httpkit.server :as http-kit]))
 
 (defstate http-server
   :start
