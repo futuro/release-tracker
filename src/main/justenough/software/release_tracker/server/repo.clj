822d048fa63799897957303dab579c222e79c4f9
Evan Niessen-Derry <eniessenderry@gmail.com>
2021-10-26 16:40:38 -0500

    Add comments to the NPE handling in repo ns
    Primarily meant to give a the Lumanu team insight into my thought process.

--- src/main/justenough/software/release_tracker/server/repo.clj
+++ src/main/justenough/software/release_tracker/server/repo.clj
@@ -10,6 +10,18 @@
              :where [_ :repo/name ?repo-name]]
            db)
       (catch NullPointerException npe
+        ;; N.B. if you attempt to query against an attribute that
+        ;; doesn't exist in the DB Asami will throw an NPE. There are
+        ;; probably other reasons this could happen, but to keep
+        ;; within a reasonable timeframe for this exercise I'm simply
+        ;; going to assume all NPEs associated with this query are
+        ;; caused in this way.
+        ;;
+        ;; I could also check the DB contents, or the connection
+        ;; `next-tx`, and make some assumptions, but I'm not going to
+        ;; because that's not a great solution for a production
+        ;; DB/this should be reported to the Asami maintainers to see
+        ;; if it's a bug or not/this is meant to be a small project.
         "No repos currently tracked.\n"))))
 
 (defn info
