7056beac46cdd213594addb704c4876e97d36d07
Evan Niessen-Derry <eniessenderry@gmail.com>
2021-10-27 21:45:40 -0500

    Add a schema to the datascript db
    This was necessary to get back-reference support for tying releases to
    repos.
    
    I also threw in a uniqueness constraint for the repo fullname to make
    it easier to reference.

--- src/main/justenough/software/release_tracker/server/database.clj
+++ src/main/justenough/software/release_tracker/server/database.clj
@@ -2,6 +2,11 @@
   (:require [datascript.core :as d]
             [mount.core :as mount :refer [defstate]]))
 
+(def schema
+  {:github.repo/releases {:db/valueType :db.type/ref
+                          :db/cardinality :db.cardinality/many}
+   :github.repo/full_name {:db/unique :db.unique/identity}})
+
 (defstate connection
   :start
-  (d/create-conn))
+  (d/create-conn schema))
