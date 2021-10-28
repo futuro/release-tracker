ee43cca884e1545a75df8b0993af10aa932ca1d7
Evan Niessen-Derry <eniessenderry@gmail.com>
2021-10-27 20:19:17 -0500

    Return repo full_names for all-repos
    This is nicer than just the short name.

--- src/main/justenough/software/release_tracker/server/repo.clj
+++ src/main/justenough/software/release_tracker/server/repo.clj
@@ -7,27 +7,10 @@
 
 (defn all-repos
   []
-  (let [db (d/db db/connection)]
-    (try
-      ;; TODO: probably this should have some different formatting.
-      (str/join " "
-             (d/q '[:find [?repo-name ...]
-                    :where [_ :github.repo/name ?repo-name]]
-                  db))
-      (catch NullPointerException npe
-        ;; N.B. if you attempt to query against an attribute that
-        ;; doesn't exist in the DB Asami will throw an NPE. There are
-        ;; probably other reasons this could happen, but to keep
-        ;; within a reasonable timeframe for this exercise I'm simply
-        ;; going to assume all NPEs associated with this query are
-        ;; caused in this way.
-        ;;
-        ;; I could also check the DB contents, or the connection
-        ;; `next-tx`, and make some assumptions, but I'm not going to
-        ;; because that's not a great solution for a production
-        ;; DB/this should be reported to the Asami maintainers to see
-        ;; if it's a bug or not/this is meant to be a small project.
-        "No repos currently tracked.\n"))))
+  (->> @db/connection
+       (d/q '[:find [?repo-name ...]
+              :where [_ :github.repo/full_name ?repo-name]])
+       (str/join " ")))
 
 (defn info
   [{:keys [user repo] :as opts}]
